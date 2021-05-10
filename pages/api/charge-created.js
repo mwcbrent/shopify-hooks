7c89a3bfffddbd0e837494856d19a21ad0abfeaf
Brent Sanders <brent@sandersnewmedia.com>
2021-05-10 14:46:01 -0400

    initial commit

---
+++ pages/api/charge-created.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export default async (req, res) => {
+  const instance = axios.create({
+    baseURL: 'https://api.rechargeapps.com/',
+    timeout: 3000,
+    headers: {'X-Recharge-Access-Token': process.env.RECHARGE_ACCESS_TOKEN}
+  });
+  console.log(req.body.charge)
+  // const scheduledDate = new Date(req.body.charge.scheduled_at)
+  const response = await instance.post(`charges/${req.body.charge.id}/change_next_charge_date`, {
+    next_charge_date: '2021-07-01'
+  })
+  return res.status(200).json(response)
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>Not much to see here...</div>
+  )
+}
